c1381025c69525b85e50a2121838164ae4cc7d58
KodiKraig <cody.craig@aol.com>
2024-04-13 16:21:57 -0400

    Add posts API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import { AuthButton } from "@/app/_components/auth";
+import { api } from "@/trpc/server";
 
 export default async function Home() {
+  const data = await api.post.getAll();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
@@ -8,6 +11,11 @@ export default async function Home() {
           Web3 <span className="text-[hsl(280,100%,70%)]">Chirp</span>
         </h1>
         <AuthButton />
+        <div>
+          {data.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </div>
     </main>
   );
