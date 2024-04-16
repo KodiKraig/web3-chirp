ff35a3e9a5cb4a5d4c9c7cf17aade19002e6e82f
KodiKraig <cody.craig@aol.com>
2024-04-16 16:53:42 -0400

    Add metadata for title and descriptions

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,3 +1,10 @@
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Profile",
+  description: "View profile information.",
+};
+
 const ProfilePage = () => {
   return (
     <main>
--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Post",
+  description: "View additional post information.",
+};
+
 const SinglePostPage = () => {
   return (
     <main>
