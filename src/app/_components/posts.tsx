d1c9488287dcb2be5b0059cf543988f67f67d1a9
KodiKraig <cody.craig@aol.com>
2024-04-16 22:20:07 -0400

    Fix broken link for profile

--- src/app/_components/posts.tsx
+++ src/app/_components/posts.tsx
@@ -28,7 +28,7 @@ export const SinglePostView: FC<PostWithUser> = ({ post, author }) => {
       <ProfileImage imageUrl={author.imageUrl} username={author.username} />
       <div className="flex flex-col">
         <div className="flex">
-          <Link href={author.username}>
+          <Link href={`/${author.username}`}>
             <span className="align-text-bottom text-slate-300">{`@${author.prettyUsername}`}</span>
           </Link>
           <div className="px-2">·</div>
